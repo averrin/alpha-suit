cc0d5ead4c98f6e701ef96ef9ad885f4fb45264e
averrin <averrin@gmail.com>
2022-11-06 15:47:25 +0100

    0.4.3

---
+++ src/view/settings/EffectEditorSettings.svelte
@@ -0,0 +1,21 @@
+<script>
+   import { SETTINGS } from "../../modules/constants.js";
+   import SettingsInput from "./SettingsInput.svelte";
+
+   import { setContext } from "svelte";
+
+   setContext("moduleId", Director.moduleId);
+</script>
+
+<div class="ui-bg-base ui-p-2 browser-settings ui-flex ui-flex-col ui-gap-2 ui-h-full">
+   <div class="ui-text-center ui-text-base-content ui-text-lg ui-font-bold">Effect Editor Settings</div>
+
+   <SettingsInput key={Director.SETTINGS.DROPPED_TILES_TO_EFFECTS} />
+   <SettingsInput key={Director.SETTINGS.PATCH_SEQ_DB_DROP} />
+
+   <SettingsInput key={Director.SETTINGS.CTRL_BLOCK_DIRECTOR} />
+   <SettingsInput key={Director.SETTINGS.SHIFT_INSTANT_EFFECT} premium={true} />
+
+   <SettingsInput key={Director.SETTINGS.HIDE_GIZMOS} />
+</div>
+
