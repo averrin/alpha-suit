77d636a077a64a447421f2b5e19c59a04fd4a96b
averrin <averrin@gmail.com>
2022-11-19 17:37:25 +0100

    0.4.14

---
+++ src/view/EditWidgetDialog.svelte
@@ -0,0 +1,22 @@
+<svelte:options accessors />
+
+<script>
+   import { v4 as uuidv4 } from "uuid";
+   import AlphaShell from "crew-components/AlphaShell";
+   import { onDestroy, tick } from "svelte";
+   import InlineButton from "crew-components/InlineButton";
+
+   import { isPremium } from "crew-components/premium";
+   import { SETTINGS, setting, onHook } from "crew-components/helpers";
+   import RemoveButton from "crew-components/RemoveButton";
+   import IconButton from "crew-components/IconButton";
+
+   export let elementRoot;
+   export let id;
+
+</script>
+
+<AlphaShell bind:elementRoot {id} fullHeight={true} temp={true}>
+  boom
+</AlphaShell>
+
